10f918c018a1f1c6119f3b605b322d8d8c6928cc
2paperstar <lee@paperst.ar>
2024-06-23 10:42:40 +0900

    feat: main page

---
+++ lib/palette.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class Palette {
+  Palette._();
+
+  static const primary = Color(0xFFFF4500);
+  static const dark = Color(0xFF252525);
+  static const grey = Color(0xFFB3B3B3);
+}
