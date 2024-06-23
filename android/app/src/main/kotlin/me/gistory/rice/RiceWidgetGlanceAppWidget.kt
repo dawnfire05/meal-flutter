cb8d4e899104fd006576aff862403aa95e92077b
2paperstar <lee@paperst.ar>
2024-06-23 23:03:38 +0900

    feat: add android widget

---
+++ android/app/src/main/kotlin/me/gistory/rice/RiceWidgetGlanceAppWidget.kt
@@ -0,0 +1,30 @@
+package me.gistory.rice
+
+import HomeWidgetGlanceState
+import HomeWidgetGlanceStateDefinition
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.glance.GlanceId
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.currentState
+import androidx.glance.layout.Column
+
+class RiceWidgetGlanceAppWidget: GlanceAppWidget() {
+    override val stateDefinition = HomeWidgetGlanceStateDefinition()
+
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            GlanceContent(context, currentState())
+        }
+    }
+
+    @Composable
+    private fun GlanceContent(context: Context, currentState: HomeWidgetGlanceState) {
+        val data = currentState.preferences
+
+        Column {
+
+        }
+    }
+}
---
+++ android/app/src/main/kotlin/me/gistory/rice/RiceWidgetReceiver.kt
@@ -0,0 +1,7 @@
+package me.gistory.rice
+
+import HomeWidgetGlanceWidgetReceiver
+
+class RiceWidgetReceiver : HomeWidgetGlanceWidgetReceiver<RiceWidgetGlanceAppWidget>() {
+    override val glanceAppWidget = RiceWidgetGlanceAppWidget()
+}
