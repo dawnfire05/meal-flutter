f54a03f467f5bce3d06d37fdf6ff5bb8e8b2e4bc
2paperstar <lee@paperst.ar>
2024-06-23 23:23:42 +0900

    feat(widget): set app group id

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:home_widget/home_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:rice/palette.dart';
@@ -8,6 +9,7 @@ void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
   Intl.defaultLocale = widgetsBinding.platformDispatcher.locale.languageCode;
+  await HomeWidget.setAppGroupId('group.me.gistory.rice');
   runApp(const MainApp());
 }
 
