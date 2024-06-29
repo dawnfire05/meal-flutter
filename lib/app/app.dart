90cc03a7520d4b89f4eb445ee1ba2ca01ffdb790
2paperstar <lee@paperst.ar>
2024-06-29 21:30:51 +0900

    refactor: separate app

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meal/palette.dart';
+import 'package:meal/screens/main_screen.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Palette.dark,
+          ),
+        ),
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
-import 'package:meal/palette.dart';
-import 'package:meal/screens/main_screen.dart';
+import 'package:meal/app/app.dart';
 
 void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting();
   Intl.defaultLocale = widgetsBinding.platformDispatcher.locale.languageCode;
   await HomeWidget.setAppGroupId('group.me.gistory.meal');
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.white,
-          titleTextStyle: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-            color: Palette.dark,
-          ),
-        ),
-      ),
-      home: const MainScreen(),
-    );
-  }
+  runApp(const App());
 }
