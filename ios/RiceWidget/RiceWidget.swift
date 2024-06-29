843fe2e11f324400e0836fee7a00e6b272a7b38a
2paperstar <lee@paperst.ar>
2024-06-29 18:53:55 +0900

    feat: ios widget design

--- ios/RiceWidget/RiceWidget.swift
+++ ios/RiceWidget/RiceWidget.swift
@@ -8,6 +8,8 @@
 import WidgetKit
 import SwiftUI
 
+private let widgetGroupId = "group.me.gistory.rice"
+
 struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
@@ -41,12 +43,17 @@ struct RiceWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
+        ZStack {
+            Text("밥").font(.system(size: 96, weight: .bold)).opacity(0.1)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("1학 아침").font(.system(size: 16, weight: .bold)).foregroundStyle(Color("primary"))
+                    Spacer()
+                    Text("Mon, June 24").font(.system(size: 8))
+                }.padding(.bottom, 1)
+                Text("흑미밥 | 미역국 | 계란후라이 | 치킨너겟 & 머스타드 | 검정콩자반 | 배추김치 | 야채샐러드 | 시리얼 & 우유 | 토스트&잼").font(.system(size: 12))
+                Spacer()
+            }
         }
     }
 }
@@ -57,7 +64,7 @@ struct RiceWidget: Widget {
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             RiceWidgetEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
+                .containerBackground(.white, for: .widget)
         }
     }
 }
@@ -77,6 +84,13 @@ extension ConfigurationAppIntent {
 }
 
 #Preview(as: .systemSmall) {
+    RiceWidget()
+} timeline: {
+    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .starEyes)
+}
+
+#Preview(as: .systemMedium) {
     RiceWidget()
 } timeline: {
     SimpleEntry(date: .now, configuration: .smiley)
