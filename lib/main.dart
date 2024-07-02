80f837be16c821048fd252d62e15ed1b1711a4e0
2paperstar <lee@paperst.ar>
2024-07-02 13:00:34 +0900

    feat: init locale

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'package:home_widget/home_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:meal/app/app.dart';
+import 'package:meal/gen/strings.g.dart';
 
 void main() async {
-  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting();
-  Intl.defaultLocale = widgetsBinding.platformDispatcher.locale.languageCode;
+  WidgetsFlutterBinding.ensureInitialized();
+  await _initLocale();
   await HomeWidget.setAppGroupId('group.me.gistory.meal');
   runApp(const App());
+}
+
+Future<void> _initLocale() async {
+  await initializeDateFormatting();
+  final locale = LocaleSettings.useDeviceLocale();
+  Intl.defaultLocale = locale.languageCode;
 }
