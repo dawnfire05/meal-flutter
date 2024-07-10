3e7f4d60dcd249a984f2bcfb3835f4b07b199409
GanghyeonSeo <48593646+GanghyeonSeo@users.noreply.github.com>
2024-07-10 18:41:03 +0900

    spalsh screen (#4)
    * chore: add flutter_native_splash
    
    * chore: flutter_native_splash:create
    
    * feat: add splash screen
    
    * modify: reflect review - android:screenOrientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
@@ -7,10 +8,12 @@ import 'package:meal/di/locator.dart';
 import 'package:meal/gen/strings.g.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await _initLocale();
   await HomeWidget.setAppGroupId('group.me.gistory.meal');
   configureDependencies();
+  FlutterNativeSplash.remove();
   runApp(const App());
 }
 
